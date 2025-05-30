614d1419eb5b8bc8bfd8b3c9c087fdfc56707f98
devlopez <devannolopez@gmail.com>
2025-05-30 13:01:29 -0500

    refactor: remove unused hero mock data and HeroWrapper component from HomePage

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,20 +1,14 @@
 // HomePage.jsx
 import { useState } from 'react';
 import NavBar from '../components/layout/NavBar';
-import HeroWrapper from '../components/sections/HeroSections/HeroWrapper';
+import Footer from '../components/layout/Footer';
 
 const HomePage = () => {
-  // For now, manually toggle for testing
-  const [isMobileView, setIsMobileView] = useState(true);
   
   return (
     <div>
       <NavBar />
-      {/* <button onClick={() => setIsMobileView(!isMobileView)} className="fixed top-4 right-4 z-50 bg-black text-white p-2">
-        Toggle View: {isMobileView ? 'Mobile' : 'Desktop'}
-      </button> */}
-      
-      <HeroWrapper isMobile={isMobileView} />
+      <Footer />
     </div>
   );
 };
