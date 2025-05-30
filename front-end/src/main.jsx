2899a6163bd0a7d36a48ca56608b02de52ccf0ec
devlopez <devannolopez@gmail.com>
2025-05-30 12:57:23 -0500

    feat: add Hanken Grotesk Variable font; update font styles and configuration

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterConfig from './router/RouterConfig';
+import './styles/fonts.css';
 import './styles/index.css';
-import './styles/font.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
