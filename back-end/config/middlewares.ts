94e1e8aea555a1c06e02c4000b1b6717f9cb7910
devlopez <devannolopez@gmail.com>
2025-06-09 19:22:02 -0500

    feat: update CORS configuration to include additional frontend URL; enhance package.json scripts for better development workflow

--- back-end/config/middlewares.ts
+++ back-end/config/middlewares.ts
@@ -8,7 +8,7 @@ module.exports = [
       // IMPORTANT: Update 'origin' to include your frontend's development URL.
       // For Vite, it's typically http://localhost:5173
       // For Create React App, it's typically http://localhost:3000
-      origin: ['http://localhost:1337', 'http://localhost:5173', 'http://localhost:3000'], // Add your frontend URL here
+      origin: ['http://localhost:1337', 'http://localhost:5173', 'http://localhost:3000', 'http://localhost:4173'], // Add your frontend URL here
       headers: '*', // Allow all headers
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], // Allow all necessary methods
       keepHeaderOnError: true,
